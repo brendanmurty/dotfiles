47ef4213693de6a4b8332f96bd449a96793f92da
Brendan Murty <b@murty.io>
2019-02-05 08:24:59 +1100

    Atom: Improved initial setup process

---
+++ .atom/setup.sh
@@ -0,0 +1,24 @@
+# Install Atom packages
+apm install atom-clock
+apm install atom-material-ui
+apm install file-icons
+apm install file-types
+apm install highlight-selected
+apm install language-blade
+apm install language-diff
+apm install language-htaccess
+apm install language-markdown
+apm install language-powershell
+apm install language-todotxt
+apm install open-files
+apm install pinned-tabs
+apm install pretty-json
+apm install rainglow
+apm install set-syntax
+
+# Copy over Atom configuration files
+cp config.cson ~/.atom
+cp init.coffee ~/.atom
+cp keymap.cson ~/.atom
+cp snippets.cson ~/.atom
+cp styles.less ~/.atom

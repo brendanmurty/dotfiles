b08ee6a893c7e4466d138d2d7a15e8f005979695
Brendan Murty <b@murty.io>
2019-02-11 16:40:27 +1100

    Core: Updated file list in undo script

--- undo.sh
+++ undo.sh
@@ -1,6 +1,7 @@
 # Revert to the previous versions of the user configuration files
 sudo mv ~/.bash_aliases.bak ~/.bash_aliases
 sudo mv ~/.bash_prompt.bak ~/.bash_prompt
+sudo mv ~/.bashrc.bak ~/.bashrc
 sudo mv ~/.gitconfig.bak ~/.gitconfig
 sudo mv ~/.gitignore.bak ~/.gitignore
 sudo mv ~/.vimrc.bak ~/.vimrc

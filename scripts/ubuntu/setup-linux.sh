650f79cf268c71c6ae284ed89487a3f65c3e0249
Brendan Murty <b@murty.io>
2019-02-14 00:21:08 +1100

    Scripts: Updated Linux setup script

--- scripts/ubuntu/setup-linux.sh
+++ scripts/ubuntu/setup-linux.sh
@@ -15,12 +15,6 @@ git config --global core.autocrlf input
 wget https://raw.githubusercontent.com/composer/getcomposer.org/d3e09029468023aa4e9dcd165e9b6f43df0a9999/web/installer -O - -q | php -- --quiet
 sudo mv composer.phar /usr/bin/composer
 
-# Install Dropbox - from https://www.dropbox.com/install-linux
-sudo dpkg -i "packages/dropbox_2018.11.28_amd64.deb"
-
-# Install Google Chrome - from https://www.google.com/chrome/
-sudo dpkg -i "packages/google-chrome-stable_current_amd64.deb"
-
 # Install Atom - from https://flight-manual.atom.io/getting-started/sections/installing-atom/#platform-linux
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
@@ -41,6 +35,8 @@ sudo apt autoclean
 # Reminder of the remaining manual steps
 echo -e "Done! \n\n"
 echo -e "Manual steps remaining: \n"
+echo -e "- Install and configure Dropbox \n"
+echo -e "- Install and configure Google Chrome \n"
 echo -e "- Setup symlinks for Git and Download files if needed \n"
 echo -e "- Add local clones of relevant Git repositories \n"
 echo -e "- Copy over relevant dotfiles to user directory \n"

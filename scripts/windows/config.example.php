04095dd2a4d40a9df683407e313c1015ba0d3efe
Brendan Murty <b@murty.io>
2019-03-10 16:44:55 +1100

    Scripts: Move exclude lists to configuration file

--- scripts/windows/config.example.php
+++ scripts/windows/config.example.php
@@ -1,6 +1,17 @@
 <?php 
 
-$to_backup = [
+$directories_to_backup = [
     ['C:\Users\Example', 'D:\Backups\Example', 'backup_example.log'],
     ['F:\Games', 'D:\Backups\Games', 'backup_games.log']
+];
+
+$exclude_directories = [
+    'AppData',
+    'Tracing',
+    'node_modules'
+];
+
+$exclude_files = [
+    'NTUSER',
+    'NTUSER*'
 ];

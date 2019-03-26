bd464811a201e8240240cd416b6a2307fc39a351
Brendan Murty <b@murty.io>
2019-03-26 15:42:46 +1100

    Scripts: macOS - Allow the backup path to be set via an environment variable

--- scripts/macos/backup-macos.sh
+++ scripts/macos/backup-macos.sh
@@ -1,15 +1,21 @@
-# Copy relevant user configuration files and folders to Dropbox
+# Copy relevant user configuration files and folders to a backup directory
 # ----
-# Configure this script to run daily at 9am as the "root" user: 
+# Edit the crontab to add a scheduled task:
 # crontab -e
-# 0 9 * * * sh /Users/USERNAME/path/to/this-file.sh
+# 
+# Then add a line to this file like (this will run daily at 9am):
+# 0 9 * * * BACKUP_DIR=/Users/USERNAME/Sync/Backup/macos-user sh /Users/USERNAME/path/to/this-file.sh
+#
+# Save and exit the file to schedule the script to run.
 # ----
 
-backupdir=/Users/USERNAME/Backups/Example
+# Exit this script if any error occurs
+set -e
 
-cp ~/.bash_aliases $backupdir
-cp ~/.bash_profile $backupdir
-cp ~/.bash_prompt $backupdir
-cp ~/.gitconfig $backupdir
-cp ~/.gitignore $backupdir
-cp ~/.vimrc $backupdir
+# Copy files to the backup directory
+cp ~/.bash_aliases $BACKUP_DIR
+cp ~/.bash_profile $BACKUP_DIR
+cp ~/.bash_prompt $BACKUP_DIR
+cp ~/.gitconfig $BACKUP_DIR
+cp ~/.gitignore $BACKUP_DIR
+cp ~/.vimrc $BACKUP_DIR

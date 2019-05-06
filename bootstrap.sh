6122251bb1c0825fc775ff0c9f8ed9d06dd508cc
Brendan Murty <b@murty.io>
2019-05-06 13:18:24 +1000

    Core: Run "setup" scripts in the loop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 # Run all "setup.sh" scripts in each subfolder besides "scripts"
 #   - Originally found here: https://github.com/rkalis/dotfiles/blob/master/bootstrap.sh
 find * -name "setup.sh" -not -wholename "scripts*" | while read setup; do
-  echo $setup
-  # ./$setup
+  ./$setup
 done
 
 # Alternative method: Manually run each relevant "setup.sh" script

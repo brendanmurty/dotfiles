61ad78ddce7fcb47f1e0a4756f9a7679ef0dd980
Brendan Murty <b@murty.io>
2019-05-07 11:35:41 +1000

    Core: Remove manual bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,29 +3,3 @@
 find * -name "setup.sh" -not -wholename "scripts*" | while read setup; do
   ./$setup
 done
-
-# Alternative method: Manually run each relevant "setup.sh" script
-#
-# cd atom
-# sh setup.sh
-# cd ..
-# 
-# cd bash
-# sh setup.sh
-# cd ..
-# 
-# cd git
-# sh setup.sh
-# cd ..
-# 
-# cd phpstorm
-# sh setup.sh
-# cd ..
-# 
-# cd vim
-# sh setup.sh
-# cd ..
-# 
-# cd vscode
-# sh setup.sh
-# cd ..

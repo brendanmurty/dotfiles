6ec73c25fb7aa70ba9fde8450095a114b4bc2462
Brendan Murty <b@murty.io>
2019-06-27 09:21:00 +1000

    Lepton: Add initial configuration file and documentation

---
+++ lepton/readme.md
@@ -0,0 +1,3 @@
+# Lepton Configuration
+
+[Lepton](http://hackjutsu.com/Lepton/) is a code snippet organiser that stores content as [GitHub Gists](https://gist.github.com/discover).
---
+++ lepton/setup.sh
@@ -0,0 +1,5 @@
+# Backup the current Lepton configuration file
+cp ~/.leptonrc ~/.leptonrc.bak
+
+# Copy over the customised Lepton configuration file
+cp custom.leptonrc ~/.leptonrc

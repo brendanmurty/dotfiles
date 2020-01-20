2344268e2c8f67424cb636f5b7de2ea0d19ffdc3
Brendan Murty <b@murty.io>
2020-01-20 11:41:27 +1100

    Script: Add Windows machine compatibility, update folder naming structure

--- scripts/media-management/photos.py
+++ scripts/media-management/photos.py
@@ -9,21 +9,23 @@
 # Requirements:
 #  - Windows machine
 #  - Python 3.8: https://www.python.org/downloads/release/python-380/
+#  - ExifRead: https://pypi.org/project/ExifRead/
 
 import sys
 import os, shutil
-import subprocess
 import os.path
+import exifread
 from datetime import datetime
 
 ######################## Functions #########################
 
 def photoDate(f):
-  "Return the date/time on which the given photo was taken."
+    "Return the date/time on which the given photo was taken."
 
-  cDate = subprocess.check_output(['sips', '-g', 'creation', f])
-  cDate = cDate.split('\n')[1].lstrip().split(': ')[1]
-  return datetime.strptime(cDate, "%Y:%m:%d %H:%M:%S")
+    img = open(f, 'rb')
+    imgTags = exifread.process_file(img)
+
+    return datetime.strptime(str(imgTags['EXIF DateTimeOriginal']), "%Y:%m:%d %H:%M:%S")
 
 ###################### Main program ########################
 
@@ -33,20 +35,14 @@ yearNow = now.year
 
 # Where the photos are and where they're going.
 sourceDir = "%s\\Dropbox\\Camera Uploads" % os.environ['USERPROFILE']
-destDir = "%s\\Dropbox\\Photos\\Brendan\\%s" % (os.environ['USERPROFILE'], yearNow)
+destDir = "%s\\Dropbox\\Photos\\Brendan" % os.environ['USERPROFILE']
 errorDir = "%s\\00 Unsorted" % destDir
-destDirSubdirs = ['01 Jan', '02 Feb', '03 Mar', '04 Apr', '05 May', '06 Jun', '07 Jul', '08 Aug', '09 Sep', '10 Oct', '11 Nov', '12 Dec', '00 Test1', '00 Test2']
 
 # Create the required directories
 if not os.path.exists(destDir):
     os.makedirs(destDir)
 if not os.path.exists(errorDir):
     os.makedirs(errorDir)
-for destDirSubdir in destDirSubdirs:
-    try:
-        os.mkdir(os.path.join(destDir, destDirSubdir))
-    except FileExistsError:
-        pass
 
 # The format for the new file names.
 fmt = "%Y-%m-%d %H-%M-%S"
@@ -66,13 +62,19 @@ lastYear = 0
 # their timestamps. If more than one photo has the same timestamp, add
 # suffixes 'a', 'b', etc. to the names. 
 for photo in photos:
-    # print("Processing %s..." % photo)
-    original = sourceDir + '/' + photo
+    original = "%s\\%s" % (sourceDir, photo)
     suffix = 'a'
+
+    # print("\nProcessing: %s" % original)
+
     try:
         pDate = photoDate(original)
+        # print("Photo date: %s" % str(pDate))
+
         yr = pDate.year
         mo = pDate.month
+        moDirectory = "%s" % datetime.strftime(pDate, '%m %b')
+        # print("\nPhoto directory: %s" % moDirectory)
 
         if not lastYear == yr or not lastMonth == mo:
             sys.stdout.write('\nProcessing %04d-%02d...' % (yr, mo))
@@ -81,26 +83,26 @@ for photo in photos:
         else:
             sys.stdout.write('.')
 
-        # TODO: Update this section to suit the "01 Jan" month folder naming convention
         newname = pDate.strftime(fmt)
-        thisDestDir = destDir + '/%04d/%02d' % (yr, mo)
+        thisDestDir = destDir + '/%04d/%s' % (yr, moDirectory)
         if not os.path.exists(thisDestDir):
             os.makedirs(thisDestDir)
+        # print("\nDestination directory: %s" % thisDestDir)
 
         duplicate = thisDestDir + '/%s.jpg' % (newname)
         while os.path.exists(duplicate):
             newname = pDate.strftime(fmt) + suffix
-            duplicate = destDir + '/%04d/%02d/%s.jpg' % (yr, mo, newname)
+            duplicate = destDir + '/%04d/%s/%s.jpg' % (yr, moDirectory, newname)
             suffix = chr(ord(suffix) + 1)
-        shutil.copy2(original, duplicate)
+        shutil.move(original, duplicate)
     except Exception:
-        shutil.copy2(original, errorDir + photo)
+        shutil.move(original, errorDir + photo)
         problems.append(photo)
     except:
         sys.exit("Execution stopped.")
 
 # Report the problem files, if any.
 if len(problems) > 0:
-    print("Problem files:")
-    print(join(problems))
-    print("These can be found in: %s" % errorDir)
+    print("\nProblem files:")
+    print("\n%s" % problems)
+    print("\nThese can be found in: %s" % errorDir)

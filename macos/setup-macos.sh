1d4ee68fe28f5ba8322e9429a103e9a40c0232a7
Brendan Murty <bmurty@deputy.com>
2021-03-10 10:16:33 +1100

    macOS: Fix Brewfile, update docs

--- macos/setup-macos.sh
+++ macos/setup-macos.sh
@@ -1,10 +1,10 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install the packages defined in "Brewfile"
+# Install the Homebrew packages defined in "Brewfile"
 brew bundle
 
-# Install the latest version of Node
+# Install and activate the latest version of Node
 nvm install node
 nvm use node
 

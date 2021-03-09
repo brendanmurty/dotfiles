f5c9626cfac05b07bf02eee8f9cbf37bdca919a9
Brendan Murty <b@murty.io>
2021-03-09 23:09:57 +1100

    macOS: Fix installation process

--- macos/setup-macos.sh
+++ macos/setup-macos.sh
@@ -1,12 +1,12 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install packages defined in "Brewfile"
+# Install the packages defined in "Brewfile"
 brew bundle
-
-# Setup custom Zsh prompt
-npm install -g spaceship-prompt
 
 # Install the latest version of Node
 nvm install node
 nvm use node
+
+# Setup custom Zsh terminal prompt
+npm install -g spaceship-prompt

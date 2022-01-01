636364bca7d06b60e73eb23f371fe71c91cb15d0
Brendan Murty <b@murty.io>
2022-01-01 14:28:45 +1100

    VS Code: Update installed packages and settings

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,2 +1,5 @@
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension GitHub.vscode-pull-request-github
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ronnidc.nunjucks

f6708083affafb006210bc2389824f1886f01d24
Brendan Murty <bmurty@deputy.com>
2022-04-08 10:59:01 +1000

    Script: Markdown to Notion importer - Fix packages and file paths

--- scripts/md2notion/md2notion-import.py
+++ scripts/md2notion/md2notion-import.py
@@ -12,7 +12,8 @@
 #
 # 1. Copy "notion.env.example" to "notion.env"
 # 2. Set appropriate values for the variables in "notion.env"
-# 3. Install required packages: `pip3 install notion md2notion python-dotenv`
+# 3. Install required packages: `pip3 install notion-cobertos-fork md2notion python-dotenv`
+#   - Temporarily using "notion-cobertos-fork" instead of "notion" due to https://github.com/Cobertos/md2notion/issues/40
 # 4. Run this script: `python3 md2notion-import.py`
 #
 
@@ -31,13 +32,13 @@ client = NotionClient(token_v2=config['NOTION_TOKEN_V2'])
 page = client.get_block(config['NOTION_IMPORT_PARENT_PAGE_URL'])
 
 # Get a list of the Markdown files in `~\Downloads\Markdown`
-sourceDir = "%s\Downloads\Markdown" % (os.environ['USERPROFILE'])
+sourceDir = "%s/Downloads/Markdown" % (os.path.expanduser('~'))
 markdownFiles = os.listdir(sourceDir)
 markdownFiles = [ x for x in markdownFiles if x[-3:] in ['.md'] ]
 
 # Import each of these Markdown files to Notion as a subpage of `NOTION_IMPORT_PARENT_PAGE_URL`
 for markdownFile in markdownFiles:
-  with open(markdownFile, "r", encoding="utf-8") as mdFile:
+  with open("%s/%s" % (sourceDir, markdownFile), "r", encoding="utf-8") as mdFile:
     newPageTitle = markdownFile[:-3]
 
     newPage = page.children.add_new(PageBlock, title=newPageTitle)

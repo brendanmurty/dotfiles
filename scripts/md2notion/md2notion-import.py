34d435f9e7203f81106bdbee952a3f018e9ddda9
Brendan Murty <bmurty@deputy.com>
2022-04-08 10:19:42 +1000

    Scripts: Initial work on Markdown to Notion importer

---
+++ scripts/md2notion/md2notion-import.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+#
+# Custom md2notion script
+# 
+# ----
+# 
+# A customised [md2notion](https://github.com/Cobertos/md2notion) script to assist in importing a directory of Markdown files to Notion.
+#
+# Imports all Markdown files in `~\Downloads\Markdown` to Notion.
+# 
+# Setup:
+#
+# 1. Copy "notion.env.example" to "notion.env"
+# 2. Set appropriate values for the variables in "notion.env"
+# 3. Install required packages: `pip install notion md2notion`
+# 4. Run this script: `python3 md2notion-import.py`
+#
+
+from notion.client import NotionClient
+from notion.block import PageBlock
+from md2notion.upload import upload
+
+# Load the configuration values from "notion.env"
+script_file_path = os.path.dirname(__file__)
+config = dotenv_values("%s\\notion.env" % script_file_path)
+
+# Setup the Notion API client
+client = NotionClient(token_v2=config['NOTION_TOKEN_V2'])
+page = client.get_block(config['NOTION_IMPORT_PARENT_PAGE_URL'])
+
+# Get a list of the Markdown files in `~\Downloads\Markdown`
+sourceDir = "%s\Downloads\Markdown" % (os.environ['USERPROFILE'])
+markdownFiles = os.listdir(sourceDir)
+markdownFiles = [ x for x in markdownFiles if x[-3:] in ['.md'] ]
+
+# Import each of these Markdown files to Notion as a subpage of `NOTION_IMPORT_PARENT_PAGE_URL`
+for markdownFile in markdownFiles:
+  with open(markdownFile, "r", encoding="utf-8") as mdFile:
+    newPageTitle = markdownFile[:-3]
+
+    newPage = page.children.add_new(PageBlock, title=newPageTitle)
+
+    upload(mdFile, newPage)
---
+++ scripts/md2notion/readme.md
@@ -0,0 +1,3 @@
+# Custom md2notion script
+
+This directory contains a customised [md2notion](https://github.com/Cobertos/md2notion) script to assist in importing a directory of Markdown files to Notion.

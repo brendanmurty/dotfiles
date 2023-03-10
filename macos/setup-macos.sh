e6b8c48c42bfe34239075f045d59188a9839ff58
Brendan Murty <brendan@boulevardglobal.com>
2023-03-10 14:21:01 +1100

    macOS: Simplify Homebrew packages list

--- macos/setup-macos.sh
+++ macos/setup-macos.sh
@@ -4,10 +4,6 @@
 # Install the Homebrew packages defined in "Brewfile"
 brew bundle
 
-# Install and activate the latest version of Node
-nvm install node
-nvm use node
-
 # Setup a simple customised Zsh terminal prompt
 cp ~/.zshrc ~/.zshrc.bak
 cp .zshrc ~/.zshrc

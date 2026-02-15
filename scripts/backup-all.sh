fc0a284f097723ff38dd49f1d8a29730e4ef30b1
Brendan Murty <brendan@murty.au>
2026-02-15 13:40:48 +1100

    Update docs

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
 # Run all backup scripts
-#  - If cron is available, setup by running 'crontab -e', then adding a line like this:
+#  - Requires a Linux system
+#  - If your system's scheduler is cron, setup by running 'crontab -e', then adding a line:
 #    0 19 * * * bash /home/username/scripts/backup-all.sh
-#  - If systemd is available, refer to 'backup-all-custom-daily.service'
+#  - If your system's scheduler is systemd, refer to 'backup-all-custom-daily.service'
 #
 
 SCRIPTS_DIR=$(dirname "$(realpath "$0")")

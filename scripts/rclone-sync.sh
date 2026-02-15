9e76b958092f85a2689a266c261690452ca19109
Brendan Murty <brendan@murty.au>
2026-02-15 16:54:00 +1100

    Move to Rclone Bisync for better sync options with remote storage

---
+++ scripts/rclone-sync.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# Setup a background local bidirectional synced copy of a Rclone remote
+#   - WARNING: The "rclone bisync" command will delete local files if they're removed
+#     from the remote! Please confirm this is what you're after, more info at:
+#     https://rclone.org/commands/rclone_bisync/
+#   - Requires a Linux system with the rclone package installed
+#   - Requires an existing Rclone remote config, more info at:
+#     https://rclone.org/commands/rclone_config
+#   - For the first run, update the rclone bisync params: add "--resync", remove "--check-access"
+#   - For subsequent runs, update the rclone bisync params: remove "--resync", add "--check-access"
+#   - Force quit all Rclone processes: sudo killall rclone
+#
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+LOG_FILE="$SCRIPT_DIR/rclone-sync.log"
+touch "$LOG_FILE"
+
+cp -n "$SCRIPT_DIR/rclone-sync.sample.env" "$SCRIPT_DIR/rclone-sync.env"
+source "$SCRIPT_DIR/rclone-sync.env"
+
+RCLONE_CHECK_FILE=".rclone.bisync.enabled.txt"
+
+touch "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+echo "WARNING - RCLONE BISYNC ENABLED" > "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+echo "Enabled by '$USER' from '$HOSTNAME'." >> "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+
+rclone touch "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+
+nohup rclone bisync \
+  "$LOCAL_SYNC_DIR" \
+  "$RCLONE_REMOTE_NAME:/" \
+  --check-access \
+  --check-filename $RCLONE_CHECK_FILE \
+  --workdir $HOME/.rclone-sync \
+  --create-empty-src-dirs \
+  --max-delete 10 \
+  --resilient \
+  --verbose \
+  --force \
+  --inplace \
+  --copy-links \
+  --conflict-resolve newer \
+  --conflict-loser num \
+  --conflict-suffix conflict \
+  --refresh-times \
+  --max-lock 2m \
+  --drive-use-trash=true \
+  --delete-after \
+  --log-file $LOG_FILE \
+  > /dev/null 2>&1 &

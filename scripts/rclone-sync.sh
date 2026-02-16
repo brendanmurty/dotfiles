0cef9425cfe35755eb4502102318b750cd64cb85
Brendan Murty <brendan@murty.au>
2026-02-16 22:33:01 +1100

    Improve resiliance of Rclone sync command

--- scripts/rclone-sync.sh
+++ scripts/rclone-sync.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #
-# Setup a background local bidirectional synced copy of a Rclone remote
+# Setup a background local bidirectional synced copy of a Rclone remote for Google Drive
 #   - WARNING: The "rclone bisync" command will delete local files if they're removed
 #     from the remote! Please confirm this is what you're after, more info at:
 #     https://rclone.org/commands/rclone_bisync/
 #   - Requires a Linux system with the rclone package installed
 #   - Requires an existing Rclone remote config, more info at:
 #     https://rclone.org/commands/rclone_config
-#   - For the first run: bash /path/to/this/rclone-sync.sh initial
-#   - For subsequent runs: bash /path/to/this/rclone-sync.sh standard
 #   - View a running log of the sync process: tail -n 50 -f ./rclone-sync.log
 #   - Force quit all Rclone processes: sudo killall rclone
 #
@@ -24,40 +22,36 @@ source "$SCRIPT_DIR/rclone-sync.env"
 # Setup sync check file
 
 RCLONE_CHECK_FILE=".rclone.bisync.enabled.txt"
+
 touch "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+rclone touch "$RCLONE_REMOTE_NAME:/$RCLONE_CHECK_FILE"
+
 echo "WARNING - RCLONE BISYNC ENABLED" > "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
-echo "Enabled by '$USER' from '$HOSTNAME'." >> "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
-rclone touch "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
+echo "Enabled by '$LOCAL_SYNC_USER' from '$HOSTNAME'." >> "$LOCAL_SYNC_DIR/$RCLONE_CHECK_FILE"
 
-# Configure the sync mode
-
-if [ "$1" == "initial" ]; then
-  SYNC_MODE="--resync"
-else
-  SYNC_MODE="--check-access"
-fi
-
-# Run the Rclone Bisync command in the background
+# Run the rclone bisync command in the background
 
 nohup rclone bisync \
   "$LOCAL_SYNC_DIR" \
   "$RCLONE_REMOTE_NAME:/" \
-  $SYNC_MODE \
+  --check-access \
   --check-filename "$RCLONE_CHECK_FILE" \
   --workdir "$HOME/.rclone-sync" \
+  --ignore-listing-checksum \
+  --resync-mode newer \
+  --recover \
   --create-empty-src-dirs \
-  --max-delete 10 \
   --resilient \
-  --verbose \
   --force \
   --inplace \
   --copy-links \
   --conflict-resolve newer \
-  --conflict-loser num \
-  --conflict-suffix conflict \
+  --conflict-loser delete \
   --refresh-times \
-  --max-lock 2m \
+  --backup-dir1 "/home/$LOCAL_SYNC_USER/.local/share/Trash/files" \
   --drive-use-trash=true \
   --delete-after \
+  --human-readable \
+  --log-level INFO \
   --log-file "$LOG_FILE" \
   > /dev/null 2>&1 &

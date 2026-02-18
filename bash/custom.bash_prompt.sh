1439167bee146e0f42443ad7fbf4100ecf60b529
Brendan Murty <brendan@foboh.com.au>
2026-02-18 17:19:35 +1100

    Improve custom Bash prompt

--- bash/custom.bash_prompt.sh
+++ bash/custom.bash_prompt.sh
@@ -1,2 +1,5 @@
 # Customised shell prompt
-PS1="\[\e[35m\]\u\[\e[m\] \[\e[36m\]\w\[\e[m\]: "
+#  - Relies on the '__git_ps1' script from the official Git repo:
+#    https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh
+#  - Download of this is also included in 'setup.sh' in this dir
+PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 " %s")'; PS1='\[\e[38;5;249m\]\u\[\e[0m\] \[\e[38;5;183m\]\W\[\e[38;5;87m\]${PS1_CMD1}\[\e[0m\]: '
--- bash/setup.sh
+++ bash/setup.sh
@@ -2,12 +2,23 @@
 
 touch "$HOME/.bash_aliases" "$HOME/.bash_profile" "$HOME/.bash_prompt"
 
+# Create backups of current Bash config files for this user
+
 cp "$HOME/.bash_aliases" "$HOME/.bash_aliases.before-dotfiles.bak"
 cp "$HOME/.bash_profile" "$HOME/.bash_profile.before-dotfiles.bak"
 cp "$HOME/.bash_prompt" "$HOME/.bash_prompt.before-dotfiles.bak"
 
+# Download the '__git_ps1' script from the official Git repo
+
+curl --output "$HOME/.git-prompt.sh" https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh
+source "$HOME/.git-prompt.sh"
+
+# Copy over the customised Bash config files
+
 cp "./custom.bash_aliases.sh" "$HOME/.bash_aliases"
 cp "./custom.bash_profile.sh" "$HOME/.bash_profile"
 cp "./custom.bash_prompt.sh" "$HOME/.bash_prompt"
+
+# Load them in to the current terminal session
 
 source "$HOME/.bash_profile"

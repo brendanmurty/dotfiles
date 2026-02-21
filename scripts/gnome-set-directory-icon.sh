31a4699aae73d081af5a06ef3dc3e48fa28732e6
Brendan Murty <brendan@foboh.com.au>
2026-02-21 13:24:36 +1100

    Update example and docs in set-directory-icon script

--- scripts/gnome-set-directory-icon.sh
+++ scripts/gnome-set-directory-icon.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 #
 # Customise a directory icon in Nautilus
+#  - To remove a custom icon: gio set -t unset "/home/bob/dir/path" metadata::custom-icon
 #
 
-DIR_PATH="$HOME/Drive"
-DIR_ICON="/usr/share/icons/Adwaita/scalable/places/"
+DIR_PATH="$HOME/Sync"
+DIR_ICON="/usr/share/icons/Yaru-prussiangreen/256x256@2x/places"
 
-gio set -t string "$DIR_PATH" metadata::custom-icon "file://$DIR_ICON/user-bookmarks.svg"
-
-# Remove custom icon
-# gio set -t unset "$DIR_PATH" metadata::custom-icon
+gio set -t string "$DIR_PATH" metadata::custom-icon "file://$DIR_ICON/user-home.png"

4ac66898212f782b3bea645601dd6e9e72924902
bmurty <bcm@murty.au>
2026-02-21 18:43:07 +1100

    Simplify user backup script

--- scripts/user-backup.sh
+++ scripts/user-backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Backup a linux User
+# Backup some of the current Linux User's config files
 #  - Makes a timestamped and password protected ZIP file of some parts of your User directory
 #  - Include and exclude rules ("x" params) are set in the "zip" command below
 #  - Saves symlinks as links, not as directories
@@ -17,48 +17,51 @@ touch "$LOG_FILE"
 cp --update=none "$SCRIPT_DIR/user-backup.env.sample" "$SCRIPT_DIR/user-backup.env"
 source "$SCRIPT_DIR/user-backup.env"
 
+SOURCE_DIR="/home/$USER"
+
 mkdir -p "$BACKUP_DIR"
 
-BACKUP_FILE="user_$(date +%Y%m%d-%H%M%S).zip"
+BACKUP_FILE="user-$(echo $USER)_$(date +%Y%m%d-%H%M%S).zip"
 BACKUP_PATH="$BACKUP_DIR/$BACKUP_FILE"
 
 # Exit if the source directory doesn't exist
 
 if [ ! -d $SOURCE_DIR ]; then
-  echo $(date "$LOG_FMT") "Error - Source directory ($SOURCE_DIR) not found, please check the path in the script" >> $LOG_FILE
+  echo $(date "$LOG_FMT") "Error - Source directory ($SOURCE_DIR) not found, please check the path in the script" >> "$LOG_FILE"
   exit 1
 fi
 
 # Start backup process
 
-echo $(date "$LOG_FMT") "Starting backup of '$SOURCE_DIR' to '$BACKUP_PATH'" >> $LOG_FILE
+echo $(date "$LOG_FMT") "Starting backup of '$SOURCE_DIR' to '$BACKUP_PATH'" >> "$LOG_FILE"
 
-mkdir -p $BACKUP_DIR
-cd $SOURCE_DIR
+mkdir -p "$BACKUP_DIR"
 
 # Create a copy of the current Grub config file
 
-cp -f "/etc/default/grub" ".grub-config.txt"
+cp -f "/etc/default/grub" "$SOURCE_DIR/.grub-config.txt"
 
 # If cron is installed, save a copy of the user's cron items
 
 if command -v crontab >/dev/null 2>&1 ; then
-  echo $(sudo crontab -l -u "$SOURCE_USER_NAME") > ".crontab-user.txt"
+  crontab -l > "$SOURCE_DIR/.crontab-user.txt"
 fi
 
 # Save a copy of the file listing, so symlink paths are logged
 
-ls -lah "$SOURCE_DIR" > ".dir-list-user.txt"
+ls -lah "$SOURCE_DIR" > "$SOURCE_DIR/.dir-list-user.txt"
 
 # Save a copy of the installed Flatpak apps
 
-flatpak list --app > ".app-list-flatpak.txt"
+flatpak list --app > "$SOURCE_DIR/.app-list-flatpak.txt"
 
 # Save a copy of the installed Snap apps
 
-snap list > ".app-list-snap.txt"
+snap list > "$SOURCE_DIR/.app-list-snap.txt"
 
 # Run the ZIP command with specific inclusions and exclusions
+
+cd $SOURCE_DIR
 
 zip \
   --symlinks \
@@ -107,4 +110,4 @@ zip \
 
 # Done
 
-echo $(date "$LOG_FMT") "Finished." >> $LOG_FILE
+echo $(date "$LOG_FMT") "Finished." >> "$LOG_FILE"

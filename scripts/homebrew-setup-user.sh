ed047c132d66a348b175dfe3f7ba8a3f8915341d
Brendan Murty <brendan@foboh.com.au>
2026-02-27 00:34:01 +1100

    Simplify Homebrew user setup script

--- scripts/homebrew-setup-user.sh
+++ scripts/homebrew-setup-user.sh
@@ -16,24 +16,15 @@ sudo chown -R $USER:$USER "$BREW_DIR_DEFAULT"
 
 sudo mv "$BREW_DIR_DEFAULT" "$BREW_DIR"
 
-echo "BREW_DIR='$(echo $BREW_DIR)'" >> "$HOME/.bash_aliases"
-echo 'alias brew="$BREW_DIR/.linuxbrew/bin/brew"' >> "$HOME/.bash_aliases"
-
-echo "BREW_DIR='$(echo $BREW_DIR)'" >> "$HOME/.bash_profile"
-echo 'export HOMEBREW_RELOCATE_BUILD_PREFIX="$BREW_DIR/.linuxbrew"' >> "$HOME/.bash_profile"
-echo 'export HOMEBREW_CELLAR="$BREW_DIR/.linuxbrew/Cellar"' >> "$HOME/.bash_profile"
-echo 'export HOMEBREW_PREFIX="$BREW_DIR/.linuxbrew"' >> "$HOME/.bash_profile"
-echo 'export HOMEBREW_NO_ENV_HINTS=1' >> "$HOME/.bash_profile"
-
-echo 'eval "$($BREW_DIR/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.bash_profile"
-
-eval "$($BREW_DIR/.linuxbrew/bin/brew shellenv)"
-
 echo "BREW_DIR='$(echo $BREW_DIR)'" >> "$HOME/.bashrc"
+echo 'export HOMEBREW_RELOCATE_BUILD_PREFIX="$BREW_DIR/.linuxbrew"' >> "$HOME/.bashrc"
+echo 'export HOMEBREW_CELLAR="$BREW_DIR/.linuxbrew/Cellar"' >> "$HOME/.bashrc"
+echo 'export HOMEBREW_PREFIX="$BREW_DIR/.linuxbrew"' >> "$HOME/.bashrc"
+echo 'export HOMEBREW_NO_ENV_HINTS=1' >> "$HOME/.bash_profile"
+echo 'eval "$($BREW_DIR/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.bashrc"
+echo 'alias brew="$BREW_DIR/.linuxbrew/bin/brew"' >> "$HOME/.bashrc"
 echo 'export PATH="$PATH:$BREW_DIR/.linuxbrew/bin"' >> "$HOME/.bashrc"
 
 source "$HOME/.bashrc"
-source "$HOME/.bash_aliases"
-source "$HOME/.bash_profile"
 
 brew install gcc

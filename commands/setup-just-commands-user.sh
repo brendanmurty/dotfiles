c49f4dc441ce26b56d57765518d613394f5ef400
bmurty <bcm@murty.au>
2026-03-07 17:29:43 +1100

    Use a 'run' alias instead of 'ujust' for 'just' runner

--- commands/setup-just-commands-user.sh
+++ commands/setup-just-commands-user.sh
@@ -3,8 +3,8 @@
 #
 # Setup 'just' command runner for the current user
 #   - Requires Homebrew (Linux or macOS) to be installed first: https://brew.sh/
-#   - Installs 'just', adds a 'ujust' alias to run the commands from 'justfile' in this dir
-#   - Then you can run from any directory (lists all options): ujust
+#   - Installs 'just', adds a 'run' alias to run the commands from 'justfile' in this dir
+#   - Then you can run from any directory (lists all options): run
 #   - More info at https://github.com/casey/just
 #
 #
@@ -17,11 +17,10 @@ echo 'Installing "just" via Homebrew'
 
 brew install just
 
-echo "Adding 'ujust' alias to '$HOME/.bash_aliases'"
+echo "Adding 'run' alias to '$HOME/.bash_aliases'"
 
 touch "$HOME/.bash_aliases"
-
-echo "alias ujust='just --justfile ${THIS_DIR}/justfile'" >> "$HOME/.bash_aliases"
+echo "alias run='just --justfile ${THIS_DIR}/justfile'" >> "$HOME/.bash_aliases"
 
 source "$HOME/.bash_aliases"
 

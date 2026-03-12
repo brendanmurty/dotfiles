c120c5f48a3b7e8af802ff50b32f26592b52d0a4
Brendan Murty <brendan@foboh.com.au>
2026-03-13 09:50:03 +1100

    Updates

--- linux/ubuntu-setup.sh
+++ linux/ubuntu-setup.sh
@@ -9,13 +9,17 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt -y install git vim
+sudo apt -y install curl zip git vim
 
 sudo select-editor
 sudo update-alternatives --config editor
 
+sudo apt -y install gnome-shell gnome-shell-extensions gnome-tweaks
+
 sudo apt -y install flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+bash "./gnome-customise.sh"
 
 echo 'Ubuntu setup script completed.'
 echo 'A system reboot is required to apply some of these updates.'

d4ccba190a6e1ab3e9f98b2e7abb65454f17a730
bmurty <bcm@murty.au>
2026-03-14 09:21:28 +1100

    Updates

--- commands/setup-just-commands-user.sh
+++ commands/setup-just-commands-user.sh
@@ -3,23 +3,25 @@
 #
 # Setup 'just' command runner for the current user
 #   - Requires Homebrew (Linux or macOS) to be installed first: https://brew.sh/
-#   - Installs 'just', adds a 'run' alias to run the commands from 'justfile' in this dir
-#   - Then you can run from any directory (lists all options): run
+#   - Installs 'just'
+#   - Adds a 'j' alias to run the commands from the file named 'justfile' in this dir
+#   - Then you can run from any directory (lists all options if no params set): j
 #   - More info at https://github.com/casey/just
 #
 #
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo 'Installing "just" via Homebrew'
+echo "Installing 'just' via Homebrew"
 
 brew install just
 
-echo "Adding 'run' alias to '$HOME/.bash_aliases'"
+echo "Adding 'j' alias to '$HOME/.bash_aliases'"
 
 touch "$HOME/.bash_aliases"
-echo "alias run='just --justfile ${THIS_DIR}/justfile'" >> "$HOME/.bash_aliases"
+
+echo "alias j='just --justfile ${THIS_DIR}/justfile'" >> "$HOME/.bash_aliases"
 
 source "$HOME/.bash_aliases"
 
-echo "Finished. You may need to restart your Terminal before the 'ujust' command works."
+echo "Finished. You may need to restart your Terminal before 'just' and the 'j' alias commands work properly."

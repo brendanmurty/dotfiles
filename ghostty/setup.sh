436d993d90abb167020ceeee5e1313d79a387cc5
Brendan Murty <brendan@foboh.com.au>
2026-03-16 14:45:56 +1100

    Updates

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -7,8 +7,12 @@
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+CONFIG_DIR="$HOME"
+
 if [[ $(uname) == "Darwin" ]]; then
   # This is a macOS machine
+  CONFIG_DIR="$HOME/Library/Application Support/com.mitchellh.ghostty"
+  mkdir -p "$CONFIG_DIR"
   brew install --cask ghostty
 else
   # Otherwise, assume this is a Linux machine using APT
@@ -17,9 +21,11 @@ else
   sudo apt install -qq --assume-yes ghostty
 fi
 
-echo "Copying 'ghostty.txt' to '$HOME/config.ghostty'"
+echo "Making a backup of '$CONFIG_DIR/config.ghostty'"
 
-touch "$HOME/config.ghostty"
-cp "$HOME/config.ghostty" "$HOME/config.ghostty.before-dotfiles.bak"
+touch "$CONFIG_DIR/config.ghostty"
+cp "$CONFIG_DIR/config.ghostty" "$CONFIG_DIR/config.ghostty.before-dotfiles.bak"
 
-cp "$THIS_DIR/ghostty.txt" "$HOME/config.ghostty"
+echo "Copying 'ghostty.txt' to '$CONFIG_DIR/config.ghostty'"
+
+cp "$THIS_DIR/ghostty.txt" "$CONFIG_DIR/config.ghostty"

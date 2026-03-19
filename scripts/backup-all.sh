158bce59b93688cd0cbd7a381d2078a39d5d809e
bmurty <bcm@murty.au>
2026-03-19 22:32:09 +1100

    Updates

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -5,7 +5,7 @@
 #  - As each of these scripts write to a log file, they've been set to continue even if one fails
 #  - If your system's scheduler is cron, setup by running 'crontab -e', then adding a line:
 #    0 19 * * * bash /home/username/scripts/backup-all.sh
-#  - If your system's scheduler is systemd, refer to 'backup-all-custom-daily.service'
+#  - If your system's scheduler is systemd, refer to 'backup-all.service'
 #
 
 SCRIPTS_DIR=$(dirname "$(realpath "$0")")

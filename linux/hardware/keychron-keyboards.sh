4d44ad5bbc295d591c39467d92f5d876ac8951b7
bmurty <bcm@murty.au>
2026-03-21 11:44:19 +1100

    Cleanup

--- linux/hardware/keychron-keyboards.sh
+++ linux/hardware/keychron-keyboards.sh
@@ -9,8 +9,7 @@ THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Allow customisation of keys, lighting and firmware via https://launcher.keychron.com/
 
-sudo cp "/etc/udev/rules.d/92-keychron.rules" "/etc/udev/rules.d/92-keychron.rules.before-dotfiles.bak"
-sudo cp "$THIS_DIR/keychron-keyboards.rules.txt"  "/etc/udev/rules.d/92-keychron.rules"
+sudo cp "$THIS_DIR/keychron-keyboards.txt"  "/etc/udev/rules.d/92-keychron.rules"
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 ibus restart

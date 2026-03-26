b375b7dc71644ea70d50220cb142d06acb8747ca
bmurty <bcm@murty.au>
2026-03-26 22:08:12 +1100

    Updates

--- linux/setup/bash.sh
+++ linux/setup/bash.sh
@@ -26,8 +26,12 @@ cp "$HOME/.bash_prompt" "$HOME/.bash_prompt.before-dotfiles.bak"
 # Download the '__git_ps1' script from the official Git repo to ~/.git_prompt.sh
 
 touch "$HOME/.git_prompt.sh"
+
 cp "$HOME/.git_prompt.sh" "$HOME/.git_prompt.sh.before-dotfiles.bak"
-curl --output "$HOME/.git_prompt.sh" "https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh"
+
+curl \
+  --output "$HOME/.git_prompt.sh" \
+  "https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh"
 
 # Copy over the customised Bash config files
 

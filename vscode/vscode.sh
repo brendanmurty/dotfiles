5e99480d9aa52e775217aecf081dc2f5bf402ff9
bmurty <bcm@murty.au>
2026-03-29 11:59:39 +1100

    Fix VS Code setup script

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -10,9 +10,11 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
 OS_NAME="$(bash $REPO_DIR/scripts/os-name.sh)"
 
-# From http://code.visualstudio.com/docs/configure/settings#_settings-file-locations
-# Assume this is a Linux system to start
+# Default to a standard install on a Linux system
 CONFIG_DIR="$HOME/.config/Code/User"
+INSTALL_CMD="code --install-extension"
+
+# Update thes
 if [[ "$OS_NAME" == "macOS" ]]; then
   # macOS system
   CONFIG_DIR="$HOME/.config/Code/User"
@@ -22,15 +24,19 @@ elif [[ "$OS_NAME" == "Windows" ]]; then
 elif [ -d "$HOME/.var/app/com.visualstudio.code" ]; then
   # Linux system with VS Code installed via Flatpak
   CONFIG_DIR="$HOME/.var/app/com.visualstudio.code/config/Code/User"
+  INSTALL_CMD="flatpak run com.visualstudio.code --install-extension"
 fi
 
 mkdir -p "$CONFIG_DIR"
 
-# Copy over config after backing up current config
+# Backup current config, copy over custom config
 
 mv "$CONFIG_DIR/settings.json" "$CONFIG_DIR/settings.json.bak"
 cp "$DIR/settings.json" "$CONFIG_DIR/settings.json"
 
 # Install all packages from the package list file in this dir
 
-xargs code --install-extension < "$DIR/vscode.extensions.txt"
+cat "$DIR/vscode.extensions.txt" | while read extension
+do
+  $INSTALL_CMD $extension
+done

e08949151f55fe9199d8eceb8e68b094237d2063
bmurty <bcm@murty.au>
2026-03-31 22:38:53 +1100

    Updates

--- dropbox/dropbox-maestral-setup.linux.sh
+++ dropbox/dropbox-maestral-setup.linux.sh
@@ -3,6 +3,7 @@
 #
 # Dropbox - Setup Maestral client app for Dropbox
 #   - More info: https://maestral.app/
+#   - Config file reference: https://maestral.app/docs/configfile
 #
 #
 
@@ -21,8 +22,19 @@ sudo apt -qq --assume-yes install \
 
 pipx install maestral[gui]
 
+mkdir -p "$HOME/Dropbox"
+
 maestral start
+maestral move-dir "$HOME/Dropbox"
+
+maestral autostart -Y
+maestral bandwidth-limit up 0
+maestral bandwidth-limit down 0
+maestral notify level SYNCISSUE
 
 maestral auth link
 
-maestral gui &
+echo ''
+echo '----'
+echo ''
+echo 'Maestral installed and configured, to open the app run: maestral gui &'

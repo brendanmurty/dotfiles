ea8567de00b1ae4c49a7f8b64ee1f6c64b317bdb
bmurty <bcm@murty.au>
2026-04-04 11:09:16 +1100

    Fixes

--- scripts/os-name.sh
+++ scripts/os-name.sh
@@ -7,17 +7,17 @@
 
 OS_NAME="$(uname -s)"
 
-if [[ "${OS_NAME}" == 'Linux' ]]; then
+if [ "${OS_NAME}" == 'Linux' ]; then
   echo "$(source /etc/os-release && echo $NAME)"
-elif [[ "${OS_NAME}" == 'Freedesktop SDK' ]]; then
+elif [ "${OS_NAME}" == 'Freedesktop SDK' ]; then
   echo "$(lsb_release -si 2>/dev/null)"
-elif [[ "${OS_NAME}" == 'Darwin' ]]; then
+elif [ "${OS_NAME}" == 'Darwin' ]; then
   echo 'macOS'
-elif [[ "${OS_NAME}" == 'CYGWIN' ]]; then
+elif [ "${OS_NAME}" == 'CYGWIN' ]; then
   echo 'Windows'
-elif [[ "${OS_NAME}" == 'MINGW' ]]; then
+elif [ "${OS_NAME}" == 'MINGW' ]; then
   echo 'Windows'
-elif [[ "${OS_NAME}" == 'MSYS_NT' ]]; then
+elif [ "${OS_NAME}" == 'MSYS_NT' ]; then
   echo 'Windows'
 else
   echo "${OS_NAME}"

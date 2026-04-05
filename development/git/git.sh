3ed77f92e611716ae40ced253e4e88c58f2a674d
bmurty <bcm@murty.au>
2026-04-05 13:43:23 +1000

    Add Git LFS setup for Ubuntu

--- development/git/git.sh
+++ development/git/git.sh
@@ -5,6 +5,13 @@
 #
 #
 
+if [[ "$OS_NAME" == "Ubuntu" ]]; then
+  # Setup Git LFS
+  curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+  sudo apt -y install git-lfs
+  git lfs install
+fi
+
 git config --global init.defaultBranch "develop"
 git config --global push.default "simple"
 git config --global push.followtags "false"

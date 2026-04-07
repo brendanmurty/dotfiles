b34124b9a6809f5518668c306728fb48f37f82ea
bmurty <bcm@murty.au>
2026-04-07 11:12:02 +1000

    Gaming script improvements

--- gaming/linux-gaming.sh
+++ gaming/linux-gaming.sh
@@ -25,7 +25,7 @@ info '==> dotfiles: start linux gaming setup'
 
 info '==> dotfiles: install packages'
 
-sudo apt update
+sudo apt update >/dev/null 2>&1
 
 sudo apt -qq --assume-yes install \
   cmake \
@@ -42,16 +42,15 @@ sudo apt -qq --assume-yes install \
   libdbus-1-dev \
   libinih-dev \
   build-essential \
-  cpufrequtils
-
-flatpak install com.vysp3r.ProtonPlus
+  cpufrequtils >/dev/null 2>&1
 
 info '==> dotfiles: setup gamemode'
 
 rm -rf "$HOME/.gamemode"
-git clone "https://github.com/FeralInteractive/gamemode.git" "$HOME/.gamemode"
+git clone --quiet "https://github.com/FeralInteractive/gamemode.git" "$HOME/.gamemode"
 cd "$HOME/.gamemode"
-bash "./bootstrap.sh"
+git checkout --quiet 1.8.2
+yes | bash "./bootstrap.sh" >/dev/null 2>&1
 
 sudo usermod -aG gamemode $(whoami)
 
@@ -68,11 +67,23 @@ xset m 0 0
 gsettings set org.gnome.desktop.interface enable-animations false
 
 # Set CPU to performance mode
-sudo cpupower frequency-set -g performance
-echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils
-sudo systemctl restart cpufrequtils
+sudo cpupower frequency-set -g performance >/dev/null 2>&1
+echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils >/dev/null 2>&1
+sudo systemctl restart cpufrequtils >/dev/null 2>&1
 
 # Increase shader size for Nvidia GPUs
 export __GL_SHADER_DISK_CACHE_SIZE=10000000000
+
+info '==> dotfiles: installing Lutris'
+
+flatpak install --reinstall -y net.lutris.Lutris
+
+info '==> dotfiles: installing ProtonPlus'
+
+flatpak install --reinstall -y com.vysp3r.ProtonPlus
+
+info '==> dotfiles: installing Steam'
+
+sudo snap install steam
 
 info '==> dotfiles: finished linux gaming setup, a system reboot is required'

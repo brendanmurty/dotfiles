3bceb6a1b77f802c77ec7c2b8949041a31ee438b
bmurty <bcm@murty.au>
2026-04-10 13:52:49 +1000

    Fixes

--- linux/packages/flatpak.sh
+++ linux/packages/flatpak.sh
@@ -17,3 +17,12 @@ fi
 # Install all packages from the package list file in this dir
 
 xargs -a "$DIR/flatpak.packages.txt" flatpak install --reinstall -y flathub
+
+# Cleanup
+
+rm -rf "$HOME/.cache/flatpak"
+mkdir -p "$HOME/.cache/flatpak"
+
+# Run update to trigger Flatpak icon and desktop file refreshing
+
+flatpak update -y

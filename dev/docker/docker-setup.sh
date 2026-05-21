a397730cd33443130140c80d4eada958d54ce461
Brendan Murty <brendan@murty.au>
2026-05-22 09:29:25 +1000

    Add Fedora support to Docker Setup script

--- dev/docker/docker-setup.sh
+++ dev/docker/docker-setup.sh
@@ -8,21 +8,41 @@
 SCRIPTS="$(cd "$(dirname "$0")" && cd ../../scripts && pwd)"
 OS_NAME="$(bash $SCRIPTS/os-name.sh)"
 
-if [[ "$OS_NAME" != "Ubuntu" ]]; then
-  echo "This script requires Ubuntu."
+if [[ "$OS_NAME" != "Fedora" && "$OS_NAME" != "Ubuntu" ]]; then
+  echo "This script requires Fedora or Ubuntu."
   exit 0
-fi
+elif [[ "$OS_NAME" == "Fedora" ]]; then
+  # Request Sudo
+  sudo -v
 
-# Add the Docker packages repository and official GPG key
+  # Install Docker CLI
+  sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+  sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo systemctl enable --now docker
 
-sudo apt update -qq
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+  # Setup Docker
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+  # Install Docker Desktop
+  DOCKER_RPM="$HOME/Downloads/temp-docker-desktop-x86_64.rpm"
+  rm -rf "$DOCKER_RPM"
+  curl --output "$DOCKER_RPM" "https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.rpm"
+  sudo dnf -y install "$DOCKER_RPM"
+  rm -rf "$DOCKER_RPM"
+elif [[ "$OS_NAME" == "Ubuntu" ]]; then
+  # Request Sudo
+  sudo -v
+
+  # Add the Docker packages repository and official GPG key
+  sudo apt update -qq
+  sudo apt install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources
+  sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -31,34 +51,27 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update -qq
+  # Install Docker and standard plugins
+  sudo apt update -qq
+  sudo apt -qq --assume-yes install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin \
+    docker-ce-rootless-extras
 
-# Install Docker and standard plugins
+  # Give this user privileged Docker access
+  sudo usermod -aG docker ${USER}
 
-sudo apt -qq --assume-yes install \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin \
-  docker-ce-rootless-extras
+  # Limit log size to avoid running out of disk
+  echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
 
-# Give this user privileged Docker access
-
-sudo usermod -aG docker ${USER}
-
-# Limit log size to avoid running out of disk
-
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
-
-# Install Docker Desktop
-
-DOCKER_DEB="$HOME/Downloads/temp-docker-desktop-amd64.deb"
-
-rm -rf "$DOCKER_DEB"
-curl --output "$DOCKER_DEB" "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb"
-
-sudo apt update -qq
-sudo apt -qq --assume-yes install "$DOCKER_DEB"
-
-rm -rf "$DOCKER_DEB"
+  # Install Docker Desktop
+  DOCKER_DEB="$HOME/Downloads/temp-docker-desktop-amd64.deb"
+  rm -rf "$DOCKER_DEB"
+  curl --output "$DOCKER_DEB" "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb"
+  sudo apt update -qq
+  sudo apt -qq --assume-yes install "$DOCKER_DEB"
+  rm -rf "$DOCKER_DEB"
+fi

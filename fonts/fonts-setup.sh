e802dbf6997eba1d1a0ddaae13f8e9b1af2adda3
Brendan Murty <brendan@murty.au>
2026-05-28 09:31:09 +1000

    Update fonts setup

--- fonts/fonts-setup.sh
+++ fonts/fonts-setup.sh
@@ -18,13 +18,20 @@ fi
 FONT_DIR="$HOME/.local/share/fonts"
 mkdir -p "$FONT_DIR"
 
-echo "Copying the font files to '$FONT_DIR'"
+echo "Copying Commit Mono fonts to '$FONT_DIR'"
 
 cp "$DIR/CommitMono/"*.otf "$FONT_DIR/"
 
+if [[ "$OS_NAME" == "Fedora" ]]; then
+  echo 'Fedora: Requesting sudo'
+  sudo -v
+
+  echo 'Fedora: Installing Google Noto Emoji fonts'
+  sudo dnf install -y google-noto-color-emoji-fonts >/dev/null 2>&1
+fi
+
 echo 'Rebuilding the font cache to make fonts available'
 
-fc-cache -f -v > /dev/null 2>&1
+fc-cache -fv > /dev/null 2>&1
 
 echo 'Fonts are ready to use.'
-

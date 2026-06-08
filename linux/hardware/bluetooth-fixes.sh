0611f409960bf9ce70c9f16eda9a28a570a7f89a
Brendan Murty <b@bcm.id.au>
2026-06-09 09:10:14 +1000

    Linux - Expand Bluetooth Fixes to also support EndeavourOS

--- linux/hardware/bluetooth-fixes.sh
+++ linux/hardware/bluetooth-fixes.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 #
 #
-# Ubuntu: Fix Bluetooth config to support high fidelity playback devices
+# Ubuntu and EndeavourOS: Fix Bluetooth config to support high fidelity playback devices
 #
 #
 
-THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$REPO"
 source "$REPO/bin/.helper.sh"
 OS="$(os)"
-BIN="$REPO/bin"
 
-if [[ "$OS" != "Ubuntu" ]]; then
-  echo "This script requires Ubuntu."
-  exit 0
+if [[ "$OS" == "Ubuntu" ]]; then
+	sudo apt -qq --assume-yes install \
+		pavucontrol \
+		pulseaudio-module-bluetooth
+
+	sudo systemctl restart bluetooth
+
+	systemctl --user --now enable wireplumber
+elif [[ "$OS" == "EndeavourOS" ]]; then
+	sudo systemctl enable --now bluetooth
+	
+	sudo pacman -S --needed bluez bluez-utils
+else
+	error 'This script requires Ubuntu or EndeavourOS'
+	exit 0
 fi
-
-sudo apt -qq update
-sudo apt -qq --assume-yes upgrade
-
-sudo apt -qq --assume-yes install \
-  pavucontrol \
-  pulseaudio-module-bluetooth
-
-sudo systemctl restart bluetooth
-
-systemctl --user --now enable wireplumber
